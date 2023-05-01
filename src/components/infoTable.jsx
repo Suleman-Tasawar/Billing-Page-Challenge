30c75a2f627751f9fbdf429b33cc195f55150090
Suleman-Tasawar <sulemantasawar9@gmail.com>
2023-05-01 12:04:50 +0500

    Working

--- src/components/infoTable.jsx
+++ src/components/infoTable.jsx
@@ -1,30 +1,5 @@
 function Table() {
-  return (
-
-    <div className="tableData">
-      {/*
-      <div className="tableItems">Date</div>
-      <div className="tableItems">Type</div>
-      <div className="tableItems">Reciept</div>
-      <div className="tableItems">1-1-1</div>
-      <div className="tableItems">Name</div>
-      <div className="tableItems">
-        <button>Download</button>
-      </div>
-      <div className="tableItems">2-1-1</div>
-      <div className="tableItems">Name</div>
-      <div className="tableItems">
-        <button>Download</button>
-      </div>
-
-      <div className="tableItems">3-1-1</div>
-      <div className="tableItems">Name</div>
-      <div className="tableItems">
-        <button>Download</button>
-      </div>
-*/}
-    </div>
-  );
+  return <div className="tableData"></div>;
 }
 
 export default Table;
